abd62556574e9abbe758708a11704f1c789e5898
Seif-Stylish <stylish1600@gmail.com>
2024-04-21 17:46:14 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,124 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+//import { TranslateService } from '@ngx-translate/core';
+import $ from "jquery";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(/*public translate: TranslateService*/) { }
+
+  /*translateCopy: any = this.translate;*/
+
+  searchValue: any = "";
+
+  users: any[] =
+    [
+      { name: "Gadon", country: "Egypt", telephone: "01016824489" },
+      { name: "Stylish", country: "Egypt", telephone: "01020386220" },
+      { name: "Monzer", country: "Egypt", telephone: "01095611137" }
+    ];
+
+  usersSearch: any[] = [];
+
+  isKeyUP: any = 0;
+
+  productsCounter: any[] = [];
+
+  products: any[] = [];
+
+  stored_products: any = [];
+
+  specified_user: any = {};
+
+  searchForUser() {
+
+    this.usersSearch = [];
+
+    if (this.searchValue.length == 0) {
+      this.isKeyUP = 1;
+      return;
+    }
+
+    for (var i = 0; i < this.users.length; i++) {
+
+      if (this.users[i].name.toLowerCase().includes(this.searchValue.toLowerCase()) || this.users[i].telephone.toLowerCase().includes(this.searchValue.toLowerCase())) {
+        this.usersSearch.push(this.users[i]);
+      }
+    }
+
+    this.isKeyUP = 1;
+
+  }
+
+  addNewProduct() {
+    this.productsCounter.push(1);
+  }
+
+  addProducts() {
+
+    localStorage.setItem("products", JSON.stringify(this.products));
+
+    this.stored_products = JSON.parse(String(localStorage.getItem("products")));
+
+    this.productsCounter = [];
+
+    this.products = [];
+  }
+
+  chooseUser(user: any) {
+    localStorage.setItem("user", JSON.stringify(user));
+    this.searchValue = "";
+    this.usersSearch = [];
+  }
+
+  addDeal() {
+    if (localStorage.getItem("user") != null && localStorage.getItem("products") != null) {
+      var storedProducts = JSON.parse(String(localStorage.getItem("products")));
+      var products = "";
+      for (var i = 0; i < storedProducts.length; i++) {
+        if (i < storedProducts.length - 1) {
+          products += storedProducts[i] + ", ";
+        }
+        else {
+          products += storedProducts[i];
+        }
+      }
+      alert("user name: " + JSON.parse(String(localStorage.getItem("user"))).name + "\n products: " + products);
+
+      localStorage.removeItem("user");
+      localStorage.removeItem("products");
+    }
+
+    else {
+      alert("you must select a user and add atleast one product");
+    }
+
+  }
+
+
+  ngOnInit(): void {
+
+    if (localStorage.getItem("products") == null) {
+      this.stored_products = [];
+    }
+
+    else {
+      this.stored_products = JSON.parse(String(localStorage.getItem("products")));
+    }
+
+    if (localStorage.getItem("user") == null) {
+      this.specified_user = {};
+    }
+    else {
+      this.specified_user = JSON.parse(String(localStorage.getItem("user")));
+    }
+
+    //throw new Error('Method not implemented.');
+  }
   title = 'sortech-exam';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule
   ],
   providers: [
     provideClientHydration()
@@ -18,3 +22,82 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule,
+    TranslateModule.forRoot(
+      {
+        defaultLanguage: "ar",
+        loader:
+        {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient]
+        }
+      }
+    )
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, "./assets/i18n/", '.json');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
